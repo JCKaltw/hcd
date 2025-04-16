392124b39245ff19482b7766ec0b5a8ea324df11
JCKalt <ckaltwasser@reminingcorp.com>
2025-04-16 09:46:21 -0400

    added run script

---
+++ bin/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source source-venv.sh
+# input file comes from dir: test
+# output file written to dir: test_done
+python src/hcd.py Butterfly\ MCD\ 8167\ D2\ ORS80646fffb17e_2504141146.xlsx 
